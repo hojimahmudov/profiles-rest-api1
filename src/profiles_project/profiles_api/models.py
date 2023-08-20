a14fbfcf3c8e9cc27bb704c9b8379dd098f65dd9
Hojiakbar <hojimahmudov1102@gmail.com>
2023-08-20 13:56:09 +0500

    Enabled the Django admin for the user profile model.

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -27,7 +27,7 @@ class UserProfileManager(BaseUserManager):
         user = self.create_user(email,name,password)
         
         user.is_superuser = True
-        user.is_stuff = True
+        user.is_staff = True
 
         user.save(using=self._db)
 
--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
